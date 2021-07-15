2b6066547cf5843bc5e0bd88247c294a71d34a6a
molneya <62799417+molneya@users.noreply.github.com>
2021-07-15 10:12:54 +0800

    fixed testkey not existing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import BB_db
 import discord, random, requests, os, time
 from discord.ext import commands, tasks
-from BB_keys import TestKey, liveKey
+from BB_keys import liveKey
 
 version = "Initial git 0.1"                 # Updated with major changes
 client = commands.Bot(command_prefix='!')
